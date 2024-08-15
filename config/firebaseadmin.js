467706a30923532695397ff6992f8953f496a88c
aperalt03 <aperalta03@tamu.edu>
2024-08-14 19:57:00 -0500

    Main

--- config/firebaseadmin.js
+++ config/firebaseadmin.js
@@ -1,11 +1,12 @@
 import admin from 'firebase-admin';
-import path from 'path';
 
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(
-            path.resolve(process.cwd(), 'config/firebase-adminsdk.json')
-        ),
+        credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        }),
     });
 }
 
